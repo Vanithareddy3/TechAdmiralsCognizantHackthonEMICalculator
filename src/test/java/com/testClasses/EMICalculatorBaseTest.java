39a8bb993e3895bfa66b5d79618a39ddaf79e83d
Vanithareddy3 <41925556+Vanithareddy3@users.noreply.github.com>
2020-06-14 23:59:51 -0400

    Update EMICalculatorBaseTest.java
    commenting

--- src/test/java/com/testClasses/EMICalculatorBaseTest.java
+++ src/test/java/com/testClasses/EMICalculatorBaseTest.java
@@ -33,8 +33,8 @@ public class EMICalculatorBaseTest extends baseTestClass {
 		landingPage.enterLoanInterestRate(testData.get("InterestRate"));
 		
 		productPage = landingPage.enterLoanTerm(testData.get("LoanTenure"));
-		productPage.clickEMIInArrears();
-		productPage.clickReadMore();
+		//productPage.clickEMIInArrears();
+		//productPage.clickReadMore();
 		productPage.fetchTestResult();
 		productPage.writeData();
 		
